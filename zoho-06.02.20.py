048b396a58f53b57bbe60e33bc121bc2207c5486
Arulprabha Raju <arulprabha.98@gmail.com>
2021-02-06 09:55:01 +0530

    Add files via upload

---
+++ zoho-06.02.20.py
@@ -0,0 +1,17 @@
+# Used Python 2
+inp = int(input())
+
+if inp > 0:
+    for i in range(inp, 0,-1):
+        
+        for j in range(inp,0,-1):
+            
+            if j >= i:
+                
+                print(j),
+            else:
+                
+                print(i),
+        
+        print
+ 
